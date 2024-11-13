671575719c0ae9f7f4ba39f46f63e3678d997ea7
בורשטיין ביילה גיטל <g0534163997@gmail.com>
2024-11-13 15:40:01 +0200

    update 2

---
+++ studio.cs
@@ -0,0 +1,10 @@
+using System.Runtime.Remoting.Messaging;
+
+const ifOpen = func (string day) =>return day+"is open";
+
+public static bool(int age)
+	{
+	if(age>18)
+	   return true;
+    return false;
+	}
