7c241b7c55555fa2c96abf853b9488a7939fced1
מרינגר דינה שירה <  @m21.local>
2024-11-13 15:39:01 +0200

    update 1

---
+++ studio.cs
@@ -0,0 +1,17 @@
+var ifOpen = func (string day) =>
+{
+	return day + "is open";
+}
+public static int toPay(int count)
+{
+	if (count > 0)
+	{
+		return 100 * count;
+	}
+	else
+		return 0;
+}
+int count = 6;
+int x = 100;
+int sum = count * x;
+int y = sum;
